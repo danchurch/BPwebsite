0b78435ee0413ee1de65313cfd904e3b09c5f028
danchurch <danchurchthomas@gmail.com>
2017-11-04 15:23:10 -0700

    hope this works

--- scripts/SB_concession_overlap/makeSBspreadsheet.py
+++ scripts/SB_concession_overlap/makeSBspreadsheet.py
@@ -28,34 +28,37 @@ C = pd.read_csv('Concessions_g.csv')
 ## get colectivas
 
 
+########### add convenio id ###############
 
-## combine them all
+## let's use convenio as our hashable id 
+
+## get a list of all convenios with a concession
+## on it:
+
+len(SBC.convenio)
+
+convenio = SBC.convenio.unique()
+
+len(convenio) ## 994
+
+########## add owner/name ##################
+
+## combine owners/names to the master dfs
 
 aa =  SBC.nombre + SBC.nombres + " " + SBC.apellidos
 aa = aa.str.replace('10','')
 aa = aa.str.strip()
-
+SBC['ComboName'] = aa
 ## get rid of the "10" values that represent NA, 
 ## that came from qgis, not sure why
 
-nom = aa
 
-## and I think that worked.
-
-nom.unique()
-
-len(nom)
-
-len(nom.unique())
-
-
-## append this combined name to SBC 
-
-SBC['ComboName'] = nom
 
 SBC[['ComboName', 'nombre']].head()
 
 SBC[['ComboName', 'nombres', 'apellidos']].tail()
+
+SBC[SBC.convenio == 'MAE-PSB-I-2012-C-021']
 
 ## looks okay. Can we do this with the SB dataframe also?
 
@@ -63,78 +66,80 @@ bb =  SB.nombre + SB.nombres + " " + SB.apellidos
 bb = bb.str.replace('10','').str.strip()
 SB['ComboName'] = bb
 
-## get rid of repeats, this will be our first column of our new df:
-nom = nom.unique()
+## check
+SB[['ComboName', 'nombre']].head()
+SB[['ComboName', 'nombres', 'apellidos']].tail()
+
+## now get a column of names/owners for each unique convenio:
+
+comboName = [ SB[SB.convenio == i ].ComboName.iloc[0] for i in convenio ]
+
+## looks right...
+
+
 
 ########### area of each BP:#########
 
 ## in HA
-SBarea = [ SB[SB.ComboName == i ].area.iloc[0]/10000 for i in nom ]
+SBarea = [ SB[SB.convenio == i ].area.iloc[0]/10000 for i in convenio ]
 
-########### now how do we get the identity of concessions in each?#########
+###########  identity of concessions in each #########
 
 ## function to collect names of concessions:
 
 def getconcIDs(cn):
-    SBs = SBC.groupby('ComboName')
+    SBs = SBC.groupby('convenio')
     bb = SBs.get_group(cn)
     conc_ids = list(bb.nam)
     return(conc_ids)
 
 ## try it out:
 
-cn = 'ASOCIACION COFRADIA HUACUPAMBA'
-
-cn = 'HOOVER SANTIAGO JIMENEZ GRANDA'
+cn = 'MAE-PSB-I-2011-C-006'
 
 getconcIDs(cn)
 
 ## seems to work
 
 ## use it, turn lists into strings
-CIDS = [ getconcIDs(i) for i in nom ]
+CIDS = [ getconcIDs(i) for i in convenio ]
 concIDs = pd.Series(CIDS).apply(lambda x : (', '.join(x)))
-
 
 
 ########### and for the combined areas of concessions?#########
 
 
 def getconcArea(sn):
-    SBs = SBC.groupby('ComboName')
+    SBs = SBC.groupby('convenio')
     bb = SBs.get_group(sn)
     concArea_Hai = sum(bb.area)/10000 ## convert to Ha
     return(concArea_Hai)
 
-sn = 'COMUNA GUAMBUZARI'
 
-sn = 'ASOCIACION DE CENTROS SHUAR TAYUNTS'
+sn = 'MAE-PSB-I-2012-C-006'
 getconcArea(sn)
 
 ## seems okay
 
 ## so for each BP:
-concArea_Ha = [ getconcArea(i) for i in nom ]
+concArea_Ha = [ getconcArea(i) for i in convenio ]
 
 
 ########### now calculate % underconcession:#########
 
 perConc = np.array(concArea_Ha) / np.array(SBarea)
 
-########### add convenio id ###############
-
-convenio = [ SB[SB.ComboName == i ].convenio for i in nom ]
 
 ####### make df #########
 
-df = pd.concat([pd.Series(nom), \
+df = pd.concat([pd.Series(comboName), \
     pd.Series(convenio),\
     pd.Series(SBarea),\
     pd.Series(concIDs),\
     pd.Series(concArea_Ha),\
     pd.Series(perConc),\
     ], keys = [ \
-        'nom', \
+        'comboName', \
         'convenio', \
         'SBarea', \
         'concIDs', \
@@ -144,6 +149,11 @@ df = pd.concat([pd.Series(nom), \
 
 ## sanity checks
 ## seems ok
+
+
+n = 'MAE-PSB-I-2011-I-055'
+
+SB[SB.convenio == n]
 
 df.to_csv('SB_percent_concessioned.csv')
 
